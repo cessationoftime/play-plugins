0eaef60d83bdc3421312a1b65008400b3cecf9b5
Chris Van Vranken <cessationoftime@gmail.com>
2012-10-22 22:03:08 -0300

    Update util/project/Build.scala

--- util/project/Build.scala
+++ util/project/Build.scala
@@ -13,7 +13,7 @@ object MinimalBuild extends Build {
 
 
   lazy val root = Project(id = "play-plugins-util", base = file("."), settings = Project.defaultSettings).settings(
-    scalaVersion := "2.10.0-M7",
+    scalaVersion := "2.10.0-RC1",
     version := buildVersion,
     publishTo <<= (version) { version: String =>
                 val nexus = "http://typesafe.artifactoryonline.com/typesafe/"
